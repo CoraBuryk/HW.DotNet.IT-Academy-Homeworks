3e4df977e6963762dc77983a04814899afc67a80
Виктория Бурик <burik.viktoriya@gmail.com>
2021-11-06 13:13:34 +0300

    Homw Work 8

---
+++ HW08.AssemblyOne/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace HW08.AssemblyOne
+{
+    class Program
+    {
+        static void Main()
+        {
+            Motorcycle moto = new Motorcycle(); //Экземпляр класса
+            int speedInt = moto.maxSpeedInternal; //Вызов поля 
+            int speedPub = moto.maxSpeedPublic; //Вызов поля
+            //int speedPriv = moto.maxSpeedPrivate; Доступен только в том типе, в котором определен
+            //int speedProt = moto.maxSpeedProtected; Доступен в типе, в котором определен и в наследниках
+
+            string ID = moto.numberID; //Вызов поля
+
+            int speed = moto.MaxSpeed; //Вызов свойства
+
+            int speedMethodPublic = moto.MotoPublic();
+            int speedMethodInternal = moto.MotoInternal();
+            int speedMethodProtectedInternal = moto.MotoProtectedInternal();
+            //int speedMethodPrivate = moto.MotoPrivate();
+            //int speedMethodProtected = moto.MotoProtected();
+            //int speedMethodPrivateProtected = moto.MotoPrivateProtected();
+
+            MotocycleInternal model = new MotocycleInternal();
+            string modelPub = model.modelPublic;
+            string modelInt = model.modelInternal;
+
+            string mod = model.Model;
+
+            string modelMethodPublic = model.ModelPublic();
+            string modelMethodInternal = model.ModelInternal();
+            string modelMethodProtectedInternal = model.ModelProtectedInternal();
+
+            //Из 4 объявленных полей можно вызвать только 2 с модификаторами доступа Internal и Public
+            //Из 6 объявленных методов можно вызвать только 3 с модификаторами доступа Public,Internal,Protected Internal
+
+            //Т.е с модификаторами доступа Private,Protected и Private Protected можно работать только в вызванном классе,
+            //в нашем случае в Motocycle. 
+        }
+    }
+}
---
+++ HW08.Travaller/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace HW08.Travaller
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Write zipcode: ");
+            string zipcode = Convert.ToString(Console.ReadLine());
+
+            string adress1 = List.Travel("Main Street St. Louisville/123", "OH 43071:/");
+            string adress2 = List.Travel("Main Long Road St. Louisville/432", "OH 43071:/");
+            string adress3 = List.Travel("High Street Pollocksville/786", "NY 56432:/");
+
+            switch (zipcode)
+            {
+                case "OH 43071:/":
+                    Console.WriteLine(adress1);
+                    Console.WriteLine(adress2);
+                    break;
+
+                case "NY 56432:/":
+                    Console.WriteLine(adress3);
+                    break;
+
+                default:
+                    Console.WriteLine(zipcode);
+                    break;
+            }
+        }
+    }
+}
