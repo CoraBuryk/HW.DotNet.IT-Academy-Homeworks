1dcf669ad073c5a71f28629923830b751dfc9689
Виктория Бурик <burik.viktoriya@gmail.com>
2021-11-06 12:58:31 +0300

    Home Work 6

---
+++ HW06.Poem/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HW06.Poem
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Write a poem with ';' after every string :");
+            foreach (string str in Console.ReadLine().Replace('o', 'a').Split(';'))
+                Console.WriteLine(str ?? "");
+        }
+    }
+}
---
+++ HW06.Strings/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HW06.Strings
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            {
+                string str1 = " ";
+                int num1 = 0;
+                int num2 = 0;
+                string str2 = " ";
+                int index1 = int.MaxValue;
+                Console.WriteLine("Write a string: ");
+                string str3 = Console.ReadLine();
+                for (int index2 = 0; index2 < str3.Length; ++index2)
+                {
+                    string[] strArray = str3.Split(' ');
+                    if (strArray.Length > num1)
+                    {
+                        num1 = strArray[index2].Length;
+                        num2 = index2;
+                        str1 = strArray[index2];
+                    }
+                }
+                Console.WriteLine(str3.Replace(str1 ?? "", " "));
+                for (int index3 = 0; index3 < str3.Length; ++index3)
+                {
+                    string[] strArray = str3.Split(' ');
+                    if (strArray.Length > num1)
+                    {
+                        num1 = strArray[index3].Length;
+                        num2 = index3;
+                        str1 = strArray[index3];
+                    }
+                    for (int index4 = 1; index4 < str3.Length; ++index4)
+                    {
+                        if (strArray.Length < index1)
+                        {
+                            index1 = strArray[index4].Length;
+                            str2 = strArray[index1];
+                        }
+                    }
+                }
+                Console.WriteLine(str3.Replace(str1 ?? "", str2 ?? "") + str3.Replace(str2 ?? "", str1 ?? ""));
+                for (int index5 = 0; index5 > str3.Length; ++index5)
+                    Console.WriteLine(str3.Replace(" ", "").Count<char>());
+                string[] array = str3.Split(' ');
+                Array.Sort<string>(((IEnumerable<string>)array).OrderBy<string, int>((Func<string, int>)(x => x.Length)).ToArray<string>());
+                Array.Reverse<string>(array);
+                foreach (string str4 in array)
+                    Console.WriteLine(str4);
+            }
+        }
+    }
+}
