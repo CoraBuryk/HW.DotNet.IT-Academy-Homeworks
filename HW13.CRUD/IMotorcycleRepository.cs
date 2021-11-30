7cdae70f39cb84e3fcd820bc386f8677e9dc6df3
Виктория Бурик <burik.viktoriya@gmail.com>
2021-11-30 14:11:56 +0300

    HomeWork 13

---
+++ HW13.CRUD/IMotorcycleRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW13.CRUD
+{
+    public interface IMotorcycleRepository
+    {
+        List<Motorcycle> GetMotorcycles();
+        public Motorcycle GetMotorcycleById(int id);
+        public void CreateMotorcycle(Motorcycle motorcycle);
+        public void DeleteMotorcycle(int id);
+        public void UpdateMotorcycle(Motorcycle motorcycle);
+    }
+}
