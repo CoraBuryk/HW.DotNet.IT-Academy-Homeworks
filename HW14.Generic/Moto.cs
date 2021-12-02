605c8d933b079329c92cb55db1dc81571113fa31
Виктория Бурик <burik.viktoriya@gmail.com>
2021-12-02 13:31:09 +0300

    Home Work 14

---
+++ HW14.Generic/Moto.cs
@@ -0,0 +1,43 @@
+﻿using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW14.Generic
+{
+    internal class Moto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public int Odometer { get; set; }
+
+        public Moto()
+        {
+
+        }
+
+        public Moto(int id, string name, string model, int year, int odometer)
+        {
+            Log.Information($"MotoRepository start");
+            if (string.IsNullOrEmpty(name) || string.IsNullOrEmpty(model))
+            {
+                throw new Exception("Empty name/model");
+            }
+            Id = id;
+            Name = name;
+            Model = model;
+            Year = year;
+            Odometer = odometer;
+        }
+        public override string ToString()
+        {
+            Log.Information($"Motorcycle.ToString()");
+            return ($"ID: {Id}, Name: {Name}, Model: {Model}, Year: {Year},Odometer: {Odometer}");
+
+        }
+    }
+}
---
+++ HW14.Queue/Car.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW14.Queue
+{
+    internal class Car
+    {
+        public string Name;
+        static Random random = new Random();
+        public int Year;
+
+        public Car()
+        {
+            Name = Faker.Name.Last();
+            Year = random.Next(1978, 2021);
+        }
+        public override string ToString()
+        {
+            return ($"{Name} {Year}");
+        }
+    }
+}
